2bf599be959bc29039c938ffc68e5731541ef184
muneebmazhar <43905103+muneebmazhar@users.noreply.github.com>
2018-11-22 10:49:06 +0500

    kk

---
+++ SchoolErp/Controllers/StudentPortalController.cs
@@ -0,0 +1,46 @@
+﻿using SchoolErp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SchoolErp.Controllers
+{
+    public class StudentPortalController : Controller
+    {
+        InvictusSchoolEntities db = new InvictusSchoolEntities();
+        // GET: StudentPortal
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult S_Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult S_Login(Student_Record data)
+        {
+
+            var rec = db.Student_Records.Where(x => x.Roll_Number == data.Roll_Number && x.Password == data.Password).SingleOrDefault();
+            Session["admin"] = rec;
+            if (Session["Admin"] != null)
+            {
+                return RedirectToAction("S_Portal");
+            }
+            else
+            {
+                ViewBag.msg = "Your User Name Or Password is Incorrect!!";
+                return View();
+            }
+        }
+        public ActionResult S_Portal()
+        {
+            return View();
+        }
+
+    }
+}
